47d157ff7b9b65a822975a90db6574479aaa20f2
yCoffeeGames <y.coffee@outlook.com>
2024-09-26 19:57:46 +0800

    Temp hide the list

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 
 A Blender add-on designed for artists
 
-* Please ignore the sync list. I mistakenly pushed the commit. I will complete it soon
-* 请忽略那个同步列表, 尚未完成, 我不小心把提交推送了, 会尽快
--- __init__.py
+++ __init__.py
@@ -100,17 +100,17 @@ class VIEW3D_PT_UEHelper(bpy.types.Panel):
         col.operator("ue.select_sync_objects",
                      text="Select sync objects", icon="EYEDROPPER")
 
-        col = layout.column()
-        row = col.row()
-        row.template_list('UE_UL_SyncList', '', context.scene,
-                          'ue_sync_list', context.scene, 'ue_sync_list_index')
-        col1 = row.column(align=True)
-        col1.operator("ue.mark_sync", text="", icon="ADD").is_remove = False
-        col1.operator("ue.mark_sync", text="", icon="REMOVE").is_remove = True
-        col1.separator()
-        col1.operator("ue.refresh_sync_list", text="",
-                     icon="FILE_REFRESH").is_first = True
-        col.operator("ue.sync_to_ue", text="Sync to UE", icon="UV_SYNC_SELECT")
+        # col = layout.column()
+        # row = col.row()
+        # row.template_list('UE_UL_SyncList', '', context.scene,
+        #                   'ue_sync_list', context.scene, 'ue_sync_list_index')
+        # col1 = row.column(align=True)
+        # col1.operator("ue.mark_sync", text="", icon="ADD").is_remove = False
+        # col1.operator("ue.mark_sync", text="", icon="REMOVE").is_remove = True
+        # col1.separator()
+        # col1.operator("ue.refresh_sync_list", text="",
+        #              icon="FILE_REFRESH").is_first = True
+        # col.operator("ue.sync_to_ue", text="Sync to UE", icon="UV_SYNC_SELECT")
 
         col = layout.column(align=True)
         col.operator("ue.toggle_face_orientation",
